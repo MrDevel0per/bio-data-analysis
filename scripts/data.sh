13f699d8d974fe6c31578bc7c591c6fb93d312a3
Owen Cruz-Abrams <mrdeveloper081@gmail.com>
2024-05-21 20:58:41 -0600

    Enable downloading of data

---
+++ scripts/data.sh
@@ -0,0 +1,25 @@
+# Usage: data.sh ARCHIVE_PATH
+# Steps:
+# 1. Download data from https://www.kaggle.com/datasets/mexwell/5m-trees-dataset
+# 2. Run this script
+if [ "$#" -ne 1 ]; then
+    echo "Usage: data.sh ARCHIVE_PATH"
+    exit 1
+fi
+ARCHIVE_PATH=$1
+# Unzip the data
+unzip $ARCHIVE_PATH -d dataset
+# Remove the archive
+rm $ARCHIVE_PATH
+# Move the data to the data directory
+mkdir -p data/trees
+mv dataset/* data/trees/
+# Remove the dataset directory
+rm -r dataset
+cd data/trees
+# Remove all non csv files
+find . -type f ! -name "*.csv" -delete
+# Remove the csv titled Column_Headers_Dryad.csv
+rm Column_Headers_Dryad.csv
+# Tell the user all done
+echo "Data has been downloaded and cleaned"
